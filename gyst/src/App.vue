2be1b70009dcb6eac3af96332697ac341175739a
gabeyong4 <gabe.yong4@gmail.com>
2022-10-23 18:35:53 +0800

    sidebar component

--- gyst/src/App.vue
+++ gyst/src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SideNavBar msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideNavBar from './components/SideNavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideNavBar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: auto;
+
+  background: #F9F9F9;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.0778245);
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.0778245);
+  box-shadow: 0px 2px 8px 0px #00000014;
+  
 }
+
+
 </style>
---
+++ gyst/src/components/SideNavBar.vue
@@ -0,0 +1,82 @@
+<template>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <div class="sidenav">
+    <div class="logo">
+    <img src="../assets/gyst_logo.png" atl = GYST>
+    </div> <br>
+    <h3> Menu </h3>
+    <div class = "menu">
+      <router-link class="button" to="">
+        <span class="material-symbols-outlined">attach_money</span>
+        <span class="text"> Budget Tracking </span>
+      </router-link> <br>
+      <router-link class="button" to="">
+        <span class="material-symbols-outlined">attach_money</span>
+        <span class="text"> Todo Goals </span>
+      </router-link>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "SideNavBar"
+
+}
+</script>
+
+<style scoped>
+
+
+/* The sidebar menu */
+.sidenav {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15px;
+    font-weight: 600;
+    line-height: 30px;
+    letter-spacing: 0px;
+    text-align: left;
+
+    min-height: 100vh; /* Full-height: remove this if you want "auto" height */
+
+    width: 250px; /* Set the width of the sidebar */
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1; /* Stay on top */
+    top: 0; /* Stay at the top */
+    left: 0;
+    background-color: white; /* White */
+    overflow-x: hidden; /* Disable horizontal scroll */
+    padding-top: 20px;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.170509);
+}
+
+/* The navigation menu links */
+.sidenav .menu {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #2C2C2C;
+  display: block;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidenav .menu:hover {
+  color: #267FCA;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
+/* @media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+} */
+
+img {
+  position: absolute;
+  width: 57px;
+  height: 43px;
+  left: 7px;
+  top: 6px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+</style>
