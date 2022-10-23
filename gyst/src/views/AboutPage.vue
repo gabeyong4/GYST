276f2bc07b21f71c5f368d009a4d3e3b70e3c85d
Parathan99 <65285193+Parathan99@users.noreply.github.com>
2022-10-23 17:26:56 +0800

    Fixed About Page Display

--- gyst/src/views/AboutPage.vue
+++ gyst/src/views/AboutPage.vue
@@ -1,11 +1,9 @@
 <template>
-  <div style="text-align:center;" v-if="user">
-    <NavBar/>
-    <h1>This is an about page</h1>
-    <h1>Welcome to GYST</h1>
-    <h3>Description of GYST: dnslfdkf</h3>
-    <LogOut/> <br><br>
-  </div>
+  <NavBar/>
+  <h1>This is an about page</h1>
+  <h1>Welcome to GYST</h1>
+  <h3>Description of GYST: dnslfdkf</h3>
+  <LogOut/> <br><br>
   
 </template>
 
