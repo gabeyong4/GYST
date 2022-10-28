f308872a168ac66a3a8d2154db1593397c715a48
gabeyong4 <gabe.yong4@gmail.com>
2022-10-28 17:12:57 +0800

    Add new row feature and retrieving document data

--- gyst/src/components/BudgetTable.vue
+++ gyst/src/components/BudgetTable.vue
@@ -1,11 +1,11 @@
 <template>
     <AgGridVue/>
-    <button @click="deselectRows">deselect rows</button>
+    <button id="addnewRows" v-on:click = "addNewRow(); redrawAllRows();"> Add New Row </button>
+    <button non="deselectRows">deselect rows</button>
     <ag-grid-vue
     class="ag-theme-alpine"
     :columnDefs = "columnDefs"
     :rowData = "rowData"
-    :gridOptions="gridOptions"
     >
     </ag-grid-vue>
 </template>
@@ -15,6 +15,17 @@
 //   import { reactive } from "vue";
   import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
   import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+  import firebaseApp from "../firebase.js";
+  import {getFirestore} from "firebase/firestore";
+  import {collection, getDocs, query, setDoc, doc} from "firebase/firestore"; // , doc, deleteDoc
+
+  const db = getFirestore(firebaseApp);
+  
+
+// var inputRow = {};
+// var gridOptions = {
+//     pinnedTopwRowData: [inputRow]
+// }
 
 //   let inputRow = {}
 
@@ -30,13 +41,59 @@
     data() {
         return {
             columnDefs: null,
-            rowData:null
+            rowData:[], 
+            detailCellRendererParams: null
         };
     },
 
     components: {
         AgGridVue,
     },
+
+    created() {
+        this.getBudget();
+        
+    },
+
+    methods: {
+        async getBudget() {
+            let z = await getDocs(query(collection(db, "Budget Tasks")));
+
+            z.forEach((doc) => {
+                this.rowData.push(doc.data())
+            })
+        },
+
+        async addNewRow() {
+            try {
+                const newRow = await setDoc(doc(db, "Budget Tasks", "1234556"),{
+                task: "",
+                amount:0,
+                date:"",
+                category:"",
+                comments:""
+            })
+                console.log(newRow)
+                alert("refresh")
+                this.$router.go()
+                // document.getElementById("myform").reset();
+                this.$emit("added")
+            }
+
+            catch(error) {
+                console.error("Error adding document ", error)
+            }
+            
+        }
+    },
+    
+
+    // mounted() {
+    //     async function display() {
+    //         let z = await getDocs(collection(db, "Budget"))
+    //         print(z)
+    //     }
+    // },
 
     beforeMount() {
         // const gridApi = ref(null); // Optional - for accessing Grid's API
@@ -55,13 +112,9 @@
             {headerName:"Date" , field:"date", editable: true, sortable: true, filter: true},
             {headerName:"Category" , field:"category", editable: true, sortable: true, filter: true},
             {headerName:"Comments" , field:"comments", editable: true, sortable: true, filter: true}
-        ];
+        ],
 
-        this.rowData = [
-            {tasks:"Poker" , amount:20, date: "1/4/2020", category: "Gambling", comments: "NIL"},
-            {tasks:"food" , amount:10, date: "4/1/2020", category: "Food", comments: "KFC"},
-            {tasks:"Mahjong" , amount:90, date: "1/1/2020", category: "Transport", comments: "NIL"}
-        ];
+        this.rowData
     }
 
         // DefaultColDef sets props common to all Columns
@@ -97,9 +150,9 @@
 <style>
 
 .table {
-float: right;
-margin: 0 1.5%;
-width: 73%;
+    float: right;
+    margin: 0 1.5%;
+    width: 73%;
 }
 
 /* .ag-root-wrapper ag-layout-normal ag-ltr {
@@ -109,10 +162,10 @@ width: 73%;
 } */
 
 .ag-theme-alpine {
-float: right;
-margin: 0 1.5%;
-width: 73%;
-height: 500px;
+    float: right;
+    margin: 0 1.5%;
+    width: 73%;
+    height: 500px;
 }
 
 .ag-header {
@@ -120,4 +173,43 @@ height: 500px;
 
 }
 
+#addnewRows {
+    float: right;
+    margin: 0 59.5%;
+    width: 15%;
+    background: #2178C0;
+    background-color: #0095ff;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    box-shadow: rgb(255 255 255 / 40%) 0 1px 0 0 inset;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    font-family: -apple-system,system-ui,"Segoe UI","Liberation Sans",sans-serif;
+    font-size: 13px;
+    font-weight: 400;
+    line-height: 1.15385;
+    outline: none;
+    padding: 8px 0.8em;
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    touch-action: manipulation;
+    vertical-align: baseline;
+    white-space: nowrap;
+}
+
+#addnewRows:hover,
+#addnewRows:focus {
+  background-color: #07c;
+}
+
+#addnewRows:focus {
+  box-shadow: 0 0 0 4px rgba(0, 149, 255, .15);
+}
+
+#addnewRows:active {
+  background-color: #0064bd;
+  box-shadow: none;
+}
 </style>
